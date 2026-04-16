cb4be111e9fa7dc6a8f0203a66d9e97170109ca9
maurakratz <kratz.maura@gmail.com>
2026-04-16 11:43:31 +0200

    uebung 1 uploaded and uebung 1 loesung completed

---
+++ uebungen/uebung_02_data_merging.R
@@ -0,0 +1,28 @@
+# QUANTITATIVE DATENANALYSE IN R | Maura Kratz | SoSe26 -----------
+# ÜBUNG 2: Data Merging                                 -----------
+# ______________________________________________________-----------
+
+
+# Aufgabe 1 ------------------------
+   # Lade dir die repräsentative Wahlstatisik herunter und lege sie in deinen
+   # .data-Unterordner. Siehe dafür auf der Kurswebseite unter dem Tab "Daten" nach.
+
+# Aufgabe 2
+   # Lade die nötigen Pakete: rio, labelled.
+   # Lese die Daten mir dem rio-Paket ein. Achte darauf, ggf. überflüssige Zeilen
+   # zu überspringen. Sieh dir das Ergebnis an.
+
+# Aufgabe 3
+   # Bereinige die Variablennamen.
+
+# Aufgabe 4
+   # Lass dir die Variablennamen ausgeben. Ändere sie zu kurzen, sinnvollen Namen um
+   # (siehe Beispiel). Füge mit labelled::set_variable_labels() den neuen Namen
+   # entsprechende Labels hinzu.
+
+# Aufgabe 5
+   # Lass dir mit summarytools eine Datensazübersicht ausgeben.
+
+# Aufgabe 6 --------------------
+   # Lade nun ebenfalls die bereinigten Strukturdaten ein (ggf. musst du sie im Skript zu Sitzung 3 noch als .RData speichern). Füge die beiden Datensätze mit einem inner_join() zusammen. Achte darauf, dass die Variablen, über die du die Datensätze zusammenfügst, in beiden Datensätzen den gleichen Namen haben. Sieh dir das Ergebnis an.
+   # Erstelle und speichere eine Übersicht des neuen Datensatzes. Speichere auch den Datensatz selbst mit als .RData Datei
